8081f4b2f99a9917f19d874617ea9980cb1f9296
babicastilho <barbara.mcastilho@gmail.com>
2024-07-23 15:26:52 +0100

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+};
---
+++ setupTests.ts
@@ -0,0 +1,4 @@
+// Imports assertion extensions from jest-dom
+import '@testing-library/jest-dom/extend-expect';
+
+// Global configurations can be added here if needed

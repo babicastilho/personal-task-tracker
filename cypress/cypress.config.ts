b6dbd04d10b01a1c085b12f0d1e1f66d719486a3
babicastilho <barbara.mcastilho@gmail.com>
2024-10-29 18:15:09 +0000

    Update Cypress baseUrl to Vercel deployment for CI/CD compatibility

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // Add e2e node event listeners here if needed
     },
-    baseUrl: 'http://localhost:3000',  // Base URL for your Next.js app
+    //baseUrl: 'http://localhost:3000',  // Base URL for your Next.js app local
+    baseUrl: 'https://personal-task-tracker-nine.vercel.app',
     supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.ts',
   },
